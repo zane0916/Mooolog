ce31940f6edd9c18606a58d05649efc640a30bdd
tlee8 <tlee8@stuy.edu>
2018-10-28 20:51:17 -0400

    fixed minor bugs

--- src/data/init_tables.py
+++ src/data/init_tables.py
@@ -1,15 +1,20 @@
 import sqlite3
 
-db = sqlite3.connect("data/Mooolog.db")
+db = sqlite3.connect("Mooolog.db")
 c = db.cursor()
 command = ""
 
-command = "DROP TABLE users"
+command = "DROP TABLE IF EXISTS users"
 c.execute(command)
 command = "CREATE TABLE IF NOT EXISTS users (user_id INTEGER PRIMARY KEY, username TEXT UNIQUE NOT NULL, password TEXT);"
 c.execute(command)
 
-command = "DROP TABLE blogs"
+command = "DROP TABLE IF EXISTS blogs"
 c.execute(command)
 command = "CREATE TABLE IF NOT EXISTS blogs (blog_id INTEGER PRIMARY KEY, user_id INTEGER, title TEXT UNIQUE NOT NULL, category TEXT, author TEXT, timestamp TEXT);"
+c.execute(command)
+
+command = "DROP TABLE IF EXISTS entries"
+c.execute(command)
+command = "CREATE TABLE IF NOT EXISTS entries (entry_id INTEGER PRIMARY KEY, blog_id INTEGER, title TEXT, content TEXT, timestamp TEXT);"
 c.execute(command)
