373bdcebef624913b355dcb76867ac183353e25e
Brian Lee <hlee5@stuy.edu>
2018-10-28 21:00:52 -0400

    Blogpage change

--- src/data/init_tables.py
+++ src/data/init_tables.py
@@ -6,15 +6,15 @@ command = ""
 
 command = "DROP TABLE IF EXISTS users"
 c.execute(command)
-command = "CREATE TABLE IF NOT EXISTS users (user_id INTEGER PRIMARY KEY, username TEXT UNIQUE NOT NULL, password TEXT);"
+command = "CREATE TABLE users (user_id INTEGER PRIMARY KEY, username TEXT UNIQUE NOT NULL, password TEXT);"
 c.execute(command)
 
 command = "DROP TABLE IF EXISTS blogs"
 c.execute(command)
-command = "CREATE TABLE IF NOT EXISTS blogs (blog_id INTEGER PRIMARY KEY, user_id INTEGER, title TEXT UNIQUE NOT NULL, category TEXT, author TEXT, timestamp TEXT);"
+command = "CREATE TABLE blogs (blog_id INTEGER PRIMARY KEY, user_id INTEGER, title TEXT UNIQUE NOT NULL, category TEXT, author TEXT, timestamp TEXT);"
 c.execute(command)
 
 command = "DROP TABLE IF EXISTS entries"
 c.execute(command)
-command = "CREATE TABLE IF NOT EXISTS entries (entry_id INTEGER PRIMARY KEY, blog_id INTEGER, title TEXT, content TEXT, timestamp TEXT);"
+command = "CREATE TABLE entries (entry_id INTEGER PRIMARY KEY, blog_id INTEGER, title TEXT, content TEXT, timestamp TEXT);"
 c.execute(command)
